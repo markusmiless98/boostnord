919b730180c284385fdcf880e82024f8f2845f53
markusmiless98 <mikael.tagt@futuregames.nu>
2021-09-15 16:45:47 +0200

    Vehicle Info Base Script
    Got nothing but two stats right now

---
+++ Assets/Scripts/Vehicles/VehicleInfoScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VehicleInfoScript : MonoBehaviour
+{
+    public int VehicleStorage = 0;
+    public float VehicleCO2Output = 1000f;
+
+    // Add stuff tomorrow uwu
+}
