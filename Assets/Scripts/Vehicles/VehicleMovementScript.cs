e407ff8ccc72e7e44bfe8ef8d68d27fecd1f33f7
markusmiless98 <mikael.tagt@futuregames.nu>
2021-09-16 12:58:44 +0200

    Minor Rotation Adjustments to Vehicle Movement
    Now vehicle turns, though when it reaches a node it briefly resets to 0.

--- Assets/Scripts/Vehicles/VehicleMovementScript.cs
+++ Assets/Scripts/Vehicles/VehicleMovementScript.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,29 +5,36 @@ public class VehicleMovementScript : MonoBehaviour
 {
     VehicleInfoScript InfoOfVehicle;
 
-    [SerializeField] Vector3 VehicleLocation;
-    Vector3[] TargetLocation;
-    Node[] nodes;
     [SerializeField] List<Node> nodeslist = new List<Node>();
 
     [SerializeField] int CurrentNodeToMoveTo = 0;
     [SerializeField] int MaxCurrentNode = 0;
 
+    float DirectionX = 0;
+    float DirectionZ = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        nodes = FindObjectOfType<PathDrawer>().transform.GetComponentsInChildren<Node>();
-        InfoOfVehicle = FindObjectOfType<VehicleInfoScript>();
-        foreach (var item in nodes)
+        // Checks for the Vehicle Information script, first in component in it, then in the scene
+        if (GetComponentInChildren<VehicleInfoScript>())
         {
-            MaxCurrentNode++;
+            ApplyVehicleInformationToMovement(GetComponent<VehicleInfoScript>());
+        }
+        else if (FindObjectOfType<VehicleInfoScript>())
+        {
+            ApplyVehicleInformationToMovement(FindObjectOfType<VehicleInfoScript>());
+        }
+        else
+        {
+            Debug.LogError("No Vehicle Information is Present in the scene");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && nodeslist != null)
+        if (Input.GetKey(KeyCode.Space) && nodeslist != null && nodeslist.Count > 0)
         {
             DoVehicleMovement();
         }
@@ -45,13 +51,22 @@ public class VehicleMovementScript : MonoBehaviour
         Debug.Log("SPACE");
     }
 
+    public void ApplyVehicleInformationToMovement(VehicleInfoScript theInformation)
+    {
+        InfoOfVehicle = theInformation;
+    }
+
+    // Moves vehicle between the different nodes
     private void DoVehicleMovement()
     {
         if (CurrentNodeToMoveTo <= MaxCurrentNode - 1)
         {
             var targetPos = nodeslist[CurrentNodeToMoveTo].transform.position;
-            VehicleLocation = targetPos;
             var MovementThisFrame = InfoOfVehicle.VehicleMovementSpeed * Time.deltaTime;
+            if (targetPos != transform.position)
+            {
+                TurnVehicleAction(targetPos);
+            }
             transform.position = Vector3.MoveTowards(transform.position, targetPos, MovementThisFrame);
             if (transform.position == targetPos)
             {
@@ -60,7 +75,37 @@ public class VehicleMovementScript : MonoBehaviour
         }
         else
         {
-            Debug.Log("W");
+            Debug.Log("Well done");
         }
+    }
+
+    private void TurnVehicleAction(Vector3 theLocation)
+    {
+        var vehicleLocation = gameObject.transform.position;
+        if (vehicleLocation.x > theLocation.x * 1.25f)
+        {
+            DirectionX = 5;
+        }
+        else if (vehicleLocation.x < theLocation.x * 0.8)
+        {
+            DirectionX = -5;
+        }
+        else
+        {
+            DirectionX /= 2;
+        }
+        if (vehicleLocation.z > theLocation.z * 1.25f)
+        {
+            DirectionZ = -5;
+        }
+        else if (vehicleLocation.z < theLocation.z * 0.8)
+        {
+            DirectionZ = 5;
+        }
+        else
+        {
+            DirectionZ /= 2;
+        }
+        transform.rotation = Quaternion.Euler(DirectionX, 90, DirectionZ);
     }
 }
